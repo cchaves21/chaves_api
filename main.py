55ddf3b653ac052d36deb80ea72f851e7c1f9499
Carlos Chaves <kchaves001@gmail.com>
2020-07-25 13:18:06 -0300

    First files upload

---
+++ main.py
@@ -0,0 +1,15 @@
+def insertUsuario(nome, email, senha):
+ 
+    return {"id": 1, "nome": nome, "senha": "*****"}
+
+
+
+def geraResponse(status, mensagem, nome_do_conteudo=False, conteudo=False):
+    response = {}
+    response["status"] = status
+    response["mensagem"] = mensagem
+
+    if(nome_do_conteudo and conteudo):
+        response[nome_do_conteudo] = conteudo
+
+    return response
---
+++ routes.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from main import insertUsuario, geraResponse
+
+app = Flask("Chaves")
+
+@app.route("/olamundo", methods=["GET"])
+def olaMundo():
+    return{"ola": "mundo"}
+
+@app.route("/cadastra/usuario", methods=["POST"])
+def cadastraUsuario():
+    
+    body = request.get_json()
+
+    if("nome" not in body):
+       return geraResponse(400, "O paramentro nome é obrigatório")
+    
+    if("email" not in body):
+       return geraResponse(400, "O paramentro email é obrigatório")
+
+    if("senha" not in body):
+       return geraResponse(400, "O paramentro senha é obrigatório")
+
+    usuario = insertUsuario(body["nome"], body["email"], body["senha"])
+
+    return geraResponse(200, "usuario criado com sucesso", "user", usuario)
+
+
+app.run()
